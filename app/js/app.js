0291d91fd0dc253d2e03da1f9f4f6c3d88c8a98b
Brandon Luong <bluong@palantir.com>
2014-07-28 16:06:32 -0700

    First terrible iteration.  Stuff is on page

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
   'ngRoute',
@@ -9,8 +8,6 @@ angular.module('myApp', [
   'myApp.directives',
   'myApp.controllers'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+    $routeProvider.when('/', {templateUrl: 'partials/appPartial.html', controller: 'MyCtrl1'});
 }]);
