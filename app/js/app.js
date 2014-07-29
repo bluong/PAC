7f047b7b64897bef69ed992121f39a1c080069d5
Brandon Luong <bluong@palantir.com>
2014-07-29 16:40:59 -0700

    RESET

--- app/js/app.js
+++ app/js/app.js
@@ -9,6 +9,5 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-    $locationProvider.html5Mode(true);
     $routeProvider.when('/', {templateUrl: 'partials/appPartial.html', controller: 'appController'});
 }]);
