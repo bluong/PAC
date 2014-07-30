56b1d82c505458cb2acc7094e64169dfb8445f3f
Brandon Luong <bluong@palantir.com>
2014-07-29 20:44:09 -0700

    First iteration complete

--- app/js/app.js
+++ app/js/app.js
@@ -8,6 +8,7 @@ angular.module('myApp', [
   'myApp.directives',
   'myApp.controllers'
 ]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+config(['$locationProvider', '$routeProvider', '$sceProvider', '$sceDelegateProvider', function($locationProvider, $routeProvider, $sceProvider, $sceDelegateProvider) {
     $routeProvider.when('/', {templateUrl: 'partials/appPartial.html', controller: 'appController'});
+    $sceDelegateProvider.resourceUrlWhitelist(['self', 'http://www.youtube.com**', "www.youtube-nocookie.com**"]);
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,8 +5,8 @@
 var app = angular.module('myApp.controllers', []);
 
 
-app.controller('appController', ['$scope', '$http', function($scope, $http) {
-    $http.get('assets/makeshift.json').then(function(response) {
+app.controller('appController', ['$scope', '$http', '$sce', function($scope, $http, $sce) {
+    $http.get('assets/selection.json').then(function(response) {
         if (response.status === 200 && response.data != null) {
             $scope.animelist = response.data;
         }
@@ -15,6 +15,18 @@ app.controller('appController', ['$scope', '$http', function($scope, $http) {
     $http.get('assets/current.json').then(function(response) {
         if (response.status === 200 && response.data != null) {
             $scope.current_anime = response.data;
+
+            $sce.trustAsResourceUrl($scope.current_anime.trailer_url);
         }
     })
+
+    $http.get('assets/announcement.json').then(function(response) {
+        if (response.status === 200 && response.data != null) {
+            $scope.announcement = response.data;
+        }
+    })
+
+    $scope.alertNotSupported = function() {
+        alert("Feature is not yet supported");
+    }
 }]);
