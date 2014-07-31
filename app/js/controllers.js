cdb6b2628cbc8df5fe1c91e2a5dedda9997b5280
Brandon Luong <bluong@palantir.com>
2014-07-30 17:10:02 -0700

    Stores trailer codes

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,7 +8,7 @@ var app = angular.module('myApp.controllers', []);
 app.controller('appController', ['$scope', '$http', '$sce', '$firebase', function($scope, $http, $sce, $firebase) {
     var emailListRef = new Firebase("https://plntr-anime-project.firebaseio.com/emails");
     var libraryRef = new Firebase("https://plntr-anime-project.firebaseio.com/library");
-    var defaultLibraryMap = {info: "", trailer_url: "", vote_count: 0, img_url: "", watched: false};
+    var defaultLibraryMap = {info: "", trailer_code: "", vote_count: 0, img_url: "", watched: false};
     $sce.trustAsResourceUrl("http://www.youtube.com/embed/**")
 
     $scope.announcement = $firebase(new Firebase("https://plntr-anime-project.firebaseio.com/announcement")).$asObject();
@@ -16,10 +16,6 @@ app.controller('appController', ['$scope', '$http', '$sce', '$firebase', functio
     $scope.animelist = $firebase(new Firebase("https://plntr-anime-project.firebaseio.com/selection")).$asArray();
 
     $scope.current_anime = $firebase(new Firebase("https://plntr-anime-project.firebaseio.com/current")).$asObject();
-
-    $scope.alertNotSupported = function() {
-        alert("Feature is not yet supported");
-    }
 
     $scope.code = $scope.current_anime.trailer_code;
 
@@ -70,6 +66,12 @@ app.controller('appController', ['$scope', '$http', '$sce', '$firebase', functio
                 newAnime[key] = value;
             }
         });
+
+        if (newAnime.trailer_url != null) {
+            newAnime.trailer_code = parseVideoId(newAnime.trailer_url);
+            delete newAnime.trailer_url
+        }
+
         var library = $firebase(libraryRef).$asArray();
         library.$loaded().then(function() {
             if (library.some(function(libraryEntry) {
@@ -82,5 +84,14 @@ app.controller('appController', ['$scope', '$http', '$sce', '$firebase', functio
             $('#submitModal').modal('hide')
             $('#submitModal').find('input').val("");
         });
+    }
+
+    function parseVideoId(youtubeURL) {
+        var video_id = youtubeURL.split('v=')[1];
+        var ampersandPosition = video_id.indexOf('&');
+        if(ampersandPosition != -1) {
+          video_id = video_id.substring(0, ampersandPosition);
+        }
+        return video_id;
     }
 }]);
